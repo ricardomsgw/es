cc4cc32ff2e4897ab8ba4fc98fbab58c235d256d
ricardomsg <ricardo.gil@tecnico.ulisboa.pt>
2020-03-08 15:31:44 +0000

    closes #3

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/JoinTournamentTest.groovy
@@ -0,0 +1,42 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentService
+import spock.lang.Specification
+
+
+class JoinTournamentSpockTest extends Specification {
+
+    def tournamentService
+
+    def setup(){
+        tournamentService = new TournamentService()
+    }
+
+    def "student joins open tournament"(){
+        //student joins tournament
+        expect: false
+    }
+
+    def "student is already in tournament"(){
+        //an exception is thrown
+        expect: false
+    }
+
+    def "student tries to join closed tournament"(){
+        //an exception is thrown
+        expect: false
+    }
+
+    def "user not a student tries to join a tournament"(){
+        //an exception is thrown
+        expect: false
+    }
+
+    def "user tries to join a tournament belonging to a course he's not enrolled in"(){
+        //an excepetion is thrown
+        expect: false
+    }
+
+
+}
