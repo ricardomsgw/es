7bed724fcef64fe070e53107cfc890f8d6da881c
ricardomsg <ricardo.gil@tecnico.ulisboa.pt>
2020-03-23 19:41:39 +0000

    closes issue #18

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -5,9 +5,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.StudentDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.UserDto;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,5 +23,11 @@ public class TournamentController {
     @PreAuthorize("hasRole('ROLE_STUDENT')")
     public TournamentDto createTournament(@RequestBody TournamentDto tournamentDto) {
         return tournamentService.createTournament(tournamentDto);
+    }
+
+    @PutMapping("/tournaments/{tournamentId}")
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    public TournamentDto addUser(@PathVariable Integer tournamentId, @Valid @RequestBody UserDto userDto) {
+        return this.tournamentService.addUser(userDto.getId(), tournamentId);
     }
 }
