0c23e527abebb728cc5f68f7a26c2c90ea17bff6
ricardomsg <ricardo.gil@tecnico.ulisboa.pt>
2020-03-27 01:01:13 +0000

    test web-service addUser closes #17

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -84,7 +84,7 @@ public class TournamentController {
 
     @PutMapping("/tournaments/{tournamentId}")
     @PreAuthorize("hasRole('ROLE_STUDENT')")
-    public TournamentDto addUser(@PathVariable Integer tournamentId, @Valid @RequestBody UserDto userDto) {
-        return this.tournamentService.addUser(userDto.getId(), tournamentId);
+    public TournamentDto addUser(@PathVariable Integer tournamentId, @Valid @RequestBody Integer userDto) {
+        return this.tournamentService.addUser(userDto, tournamentId);
     }
 }
